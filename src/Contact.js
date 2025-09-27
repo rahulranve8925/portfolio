193b4f3aff77552ec9985aacce4715a321bf0f6e
rahulranve8925 <rahulranve8925@gmail.com>
2025-09-27 23:46:40 +0530

    Initial commit - Portfoli template

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>Email: rahulranve8925@gmail.com</p>
+      <p>Phone: +91-89753-34234</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/Projects.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <ul>
+        <li>Movie Recommendation Engine - React</li>
+        <li>Developer Portal - React & TypeScript</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
